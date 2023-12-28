9ed24c7a51728825cb1866467cde0a07cd85e15a
shashvatms <119059021+shashvatms@users.noreply.github.com>
2023-12-28 20:52:18 +0530

    Create merge two sorted arrays.cpp

---
+++ Arrarys/searching and sorting/merge two sorted arrays.cpp
@@ -0,0 +1,70 @@
+Problem
+Result
+Code Merge Two Sorted Arrays
+Send Feedback
+You have been given two sorted arrays/lists(ARR1 and ARR2) of size N and M respectively, merge them into a third array/list such that the third array is also sorted.
+Note:
+Input Format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+Second line contains 'N' single space separated integers representing the elements of the first array/list.
+
+Third line contains an integer 'M' representing the size of the second array/list.
+
+Fourth line contains 'M' single space separated integers representing the elements of the second array/list.
+
+Output Format :
+For each test case, print the sorted array/list(of size N + M) in a single row, separated by a single space.
+
+Output for every test case will be printed in a separate line.
+
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+0 <= M <= 10^5
+Time Limit: 1 sec 
+
+Note:
+Consider the case when size of the two arrays is not same.
+
+Sample Input 1 :
+1
+5
+1 3 4 7 11
+4
+2 4 6 13
+
+Sample Output 1 :
+1 2 3 4 4 6 7 11 13 
+
+Sample Input 2 :
+2
+3
+10 100 500
+7
+4 7 9 25 30 300 450
+4
+7 45 89 90
+0
+
+Sample Output 2 :
+4 7 9 10 25 30 100 300 450 500
+7 45 89 90
+
+
+void merge(int arr1[], int size1, int arr2[],int size2, int ans[]){
+    int na1=0,na2=0,nans=0;
+    for(int i=0;i<size1+size2;i++){
+        if(arr1[na1]<=arr2[na2]){
+            ans[nans]=arr1[na1];
+            na1 = na1 + 1;
+        }
+        else{
+            ans[nans]=arr2[na2];
+            na2 = na2 + 1;
+        }
+        nans = nans + 1;
+    }
+}
