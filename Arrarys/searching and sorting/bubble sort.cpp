006afc272fe07576a0ae9f7dc08eff772bcdb91a
shashvatms <119059021+shashvatms@users.noreply.github.com>
2023-12-28 20:51:37 +0530

    Create bubble sort.cpp

---
+++ Arrarys/searching and sorting/bubble sort.cpp
@@ -0,0 +1,11 @@
+void bubbleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
