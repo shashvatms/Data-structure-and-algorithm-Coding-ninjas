4390d585a8fe6615987861f3480615dd3d0bb7b6
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-04 21:19:20 +0530

    Create  Check Permutation.cpp

---
+++ Arrarys/charachter and 2d array/Check Permutation.cpp
@@ -0,0 +1,39 @@
+ Check Permutation
+Easy
+Score
+69.6/80
+Average time to solve is 20m
+Problem statement
+Send feedback
+
+For a given two strings, 'str1' and 'str2', check whether they are a permutation of each other or not.
+
+Permutations of each other
+Two strings are said to be a permutation of each other when either of the string's characters can be rearranged so that it becomes identical to the other one.
+
+Example: 
+str1= "sinrtg" 
+str2 = "string"
+
+The character of the first string(str1) can be rearranged to form str2 and hence we can say that the given strings are a permutation of each other.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+
+Sample Input 1:
+abcde
+baedc
+
+Sample Output 1:
+true
+
+Sample Input 2:
+abc
+cbd
+
+Sample Output 2:
+false
