4236b7c93b35b66a129a881a5d2997359f17b490
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-10 22:23:02 +0530

    Create  Find the highest.cpp

---
+++ pointer/Find the highest.cpp
@@ -0,0 +1,31 @@
+ Find the highest
+Easy
+Score
+80/80
+Average time to solve is 20m
+Problem statement
+Send feedback
+
+Given an array of n integers, find and return the pointer type reference to the highest element of the array. For example if the array is "1 2 3 4 5", you have to return the pointer type variable holding the address of "5".
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints
+ 1<= n <=10^6
+-10^6 <= ar[i] <= 10^6
+
+Note
+You just have to write the definiton of the given function template and return the corresponding pointer type reference. No need of taking any input or printing any output.
+
+Sample Input 1
+ 2
+-56 10
+
+Sample Output
+10
+
+Sample Input 2
+4
+1 2 3 4
+
+Sample Output 2
+4
