5d8617e33dd9f4bf89cae08a8c531c11a4bbf1fc
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-10 22:25:08 +0530

    Create  Reverse an array.cpp

---
+++ pointer/Reverse an array.cpp
@@ -0,0 +1,27 @@
+ Reverse an array
+Moderate
+Score
+160/160
+Average time to solve is 40m
+Problem statement
+Send feedback
+
+Given an array ar of n integers, reverse the elements of the array by using the concept of pointers.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints
+1<= n <=10^6
+-10^6<= ar[i] <= 10^6
+
+Note
+ You only have to complete the definition of the given function named "reverse". Input has already been taken for you.
+
+Sample Input :
+4
+1
+2
+3
+4
+
+Sample Output :
+4 3 2 1 
