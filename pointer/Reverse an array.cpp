1266d56132d8dad535b1c72e486cc05159859113
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-11 21:44:50 +0530

    Update Reverse an array.cpp

--- pointer/Reverse an array.cpp
+++ pointer/Reverse an array.cpp
@@ -25,3 +25,33 @@ Sample Input :
 
 Sample Output :
 4 3 2 1 
+
+
+ 
+#include <iostream>
+using namespace std;
+void reverse(int *ar,int n)
+{
+    int j =0;
+    for(int i=n-1;i>=0;i--)
+    {
+        *(ar+j)=*(ar+i);
+        j++;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    reverse(ar,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<ar[i]<<" ";
+    }
+    return 0;
+}
