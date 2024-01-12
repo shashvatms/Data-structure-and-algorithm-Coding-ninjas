257cd5dab65d6afa5ac9fe4d9de2963c57968784
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-12 22:19:05 +0530

    Create  Count Pairs.cpp

---
+++ Standard Tempelate Library/Count Pairs.cpp
@@ -0,0 +1,22 @@
+ Count Pairs
+Easy
+Average time to solve is 20m
+Problem statement
+Send feedback
+
+You have been given a vectors of pairs. Your task is to complete the function given which counts the pairs which have first value greater than their second value and returns the count.
+
+Note:
+You don't have to print anything it has been already taken care of. Just complete the function as required and return the count.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1
+5
+1 2
+3 0
+7 5
+8  4
+10 9
+
+Output 1
+4
