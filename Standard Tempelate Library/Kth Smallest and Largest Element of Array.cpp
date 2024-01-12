b4e25c2259b056c5f0494c902c87e2fbc02bb7da
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-12 22:15:40 +0530

    Update Kth Smallest and Largest Element of Array.cpp

--- Standard Tempelate Library/Kth Smallest and Largest Element of Array.cpp
+++ Standard Tempelate Library/Kth Smallest and Largest Element of Array.cpp
@@ -52,3 +52,78 @@ Sample Input 2:
 Sample Output 2:
  2 2
  1 5
+
+
+ 
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "solution.h"
+
+class Runner
+{
+    int t;
+    vector<vector<int>> arrs;
+    vector<int> ns, ks;
+
+public:
+    void takeInput()
+    {
+        cin >> t;
+        for (int i = 0; i < t; i++)
+        {
+            int n, k;
+            cin >> n >> k;
+
+            vector<int> arr(n);
+            for(int i = 0; i < n; i++) 
+            {
+                cin >> arr[i];
+            }
+
+            ns.push_back(n);
+            ks.push_back(k);
+            arrs.push_back(arr);
+        }
+    }
+
+    void execute()
+    {
+        vector<vector<int>> arrsCopy = arrs;
+        vector<int> nsCopy = ns;
+        vector<int> ksCopy = ks;
+        for (int i = 0; i < t; i++)
+        {
+            vector<int> result = kthSmallLarge(arrsCopy[i], nsCopy[i], ksCopy[i]);
+        }
+    }
+
+    void executeAndPrintOutput()
+    {
+        for (int i = 0; i < t; i++)
+        {
+            vector<int> result = kthSmallLarge(arrs[i], ns[i], ks[i]);
+            cout << result[0] << ' ' << result[1] << '\n';
+        }
+    }
+};
+#include<algorithm>
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+    sort(arr.begin(),arr.end());
+    vector<int> narr;
+    narr.push_back(arr[k-1]);
+    narr.push_back(arr[n-k]);
+    return narr;
+}
+int main()
+{   
+    Runner runner;
+    runner.takeInput();
+    runner.executeAndPrintOutput();
+    return 0;
+}
