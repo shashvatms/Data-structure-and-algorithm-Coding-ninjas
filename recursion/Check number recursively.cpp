0efa2da2aa962d4f50b670cfb94ccd46eb8aef5e
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-13 22:43:58 +0530

    Create   Check number recursively.cpp

---
+++ recursion/Check number recursively.cpp
@@ -0,0 +1,32 @@
+
+  Check number recursively
+Easy
+Score
+80/80
+Average time to solve is 20m
+Problem statement
+Send feedback
+
+Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+
+Do this recursively.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= N <= 10^3
+
+Sample Input 1 :
+3
+9 8 10
+8
+
+Sample Output 1 :
+true
+
+Sample Input 2 :
+3
+9 8 10
+2
+
+Sample Output 2 :
+false
