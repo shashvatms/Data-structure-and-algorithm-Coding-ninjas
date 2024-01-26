1c0a9b3fe202948577455a24779328a997f459d1
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-26 22:47:08 +0530

    Create  Node with largest data.cpp

---
+++ Tree/Node with largest data.cpp
@@ -0,0 +1,136 @@
+ Node with largest data
+Moderate
+Score
+120/160
+Average time to solve is 40m
+Problem statement
+Send feedback
+
+Given a generic tree, find and return the node with maximum data. You need to return the node which is having maximum data.
+
+Return null if tree is empty.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+Time Limit: 1 sec
+
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+
+Sample Output 1:
+50
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+TreeNode<int>* maxDataNode(TreeNode<int>* root) {
+    TreeNode<int>* Node = root;
+    int max = root->data;
+    for(int i=0;i<root->children.size();i++){
+        TreeNode<int>* temp = maxDataNode(root->children[i]);
+        if(temp->data>max){
+            max = temp->data;
+            Node = temp;
+        }
+    }
+    return Node;
+    
+}
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    TreeNode<int>* ans = maxDataNode(root);
+
+    if (root != NULL) {
+        cout << ans->data;
+    }
+}
